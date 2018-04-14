43168d64178a5f08d27c97ae68bcf93039bf006b
Jonathan Barnes <jonathan@codeword.net>
2018-04-14 08:42:57 -0700

    add image & font support to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,22 @@ module {
       }),
       test: /\.less$/
     },
+    {
+      test: /\.jpe?g$|\.gif$|\.ico$|\.png$|\.svg$/,
+      use: 'file-loader?name=[name].[ext]?[hash]'
+    },
+    {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      use: 'url-loader?limit=10000&mimetype=application/font-woff'
+    },
+    {
+      test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      use: 'file-loader'
+    },
+    {
+      test: /\.otf(\?.*)?$/,
+      use: 'file-loader?name=/fonts/[name].[ext]&mimetype=application/font-otf'
+    },
   ]
 },
 plugins: [
